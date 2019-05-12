4ab5b8cc1e3a465c87f404c4efd4f7745b96ccf5
Caio Ramos <caioramos97@gmail.com>
2019-05-12 12:08:25 -0300

    Analise dos resultados das tabelas sql

---
+++ analysis/query_issues.sql
@@ -0,0 +1,18 @@
+select
+	i.kee as uuid,
+	i.severity,
+	i.message as message,
+	i.line as line,
+	p.name as file_name,
+	m.name as metric,
+	l.value as value
+from
+	issues i 
+	inner join projects p on i.component_uuid = p.uuid
+	inner join live_measures l on i.component_uuid = l.component_uuid
+	inner join metrics m on l.metric_id = m.id
+where
+	i.kee in ( -- uuid's das dívidas técnicas com tipo BLOCKER ou CRITICAL
+		(select kee from issues where severity IN ('BLOCKER', 'CRITICAL'))
+	)
+	and l.metric_id in (3, 18) -- metricas que se deseja extrair do arquivo em questao
---
+++ analysis/useful_queries.sql
@@ -0,0 +1,5 @@
+select * from issues where kee = 'AWqfjDf3p3SSQDVK5z3O'
+
+select * from projects where uuid = 'AWqfjCTLp3SSQDVK5zZH'
+
+select * from live_measures where component_uuid = 'AWqfjCTLp3SSQDVK5zZH' and metric_id = 18
